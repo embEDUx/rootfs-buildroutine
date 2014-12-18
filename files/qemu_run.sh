95c221ff47e409ff507ae537a7a170e851e0e37b
Stefan Junker <mail@stefanjunker.de>
2014-12-18 21:14:37 +0100

    Add PID handling to qemu_run

--- files/qemu_run.sh
+++ files/qemu_run.sh
@@ -15,15 +15,19 @@
 #  -fsdev local,id=root,path=/var/tmp/embedux/rootfs/,security_model=mapped-file \
 #  -device virtio-9p-device,fsdev=root,mount_tag=/dev/root
 
+PIDFILE=${EMBEDUX_TMP}/qemu.pid
+IMAGE=${EMBEDUX_TMP}/rootfs.btrfs.img
+
 QEMU_AUDIO_DRV=none qemu-system-arm \
   -M virt \
   -m 3072 \
   -daemonize \
+  -pidfile ${PIDFILE} \
   -kernel zImage \
   -append "root=/dev/vda rw rootflags=subvol=qemu_prepare console=ttyAMA0" \
   -net user \
   -netdev user,id=vnet0 \
   -redir tcp:2222::22 \
   -device virtio-net-device,netdev=vnet0 \
-  -drive file=${EMBEDUX_TMP}/rootfs.btrfs.img,id=root \
+  -drive file=${IMAGE},id=root \
   -device virtio-blk-device,drive=root
