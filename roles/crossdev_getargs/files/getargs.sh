1a720052cd8c29946bec47a63b3d42dd7ad52db2
Stefan Junker <mail@stefanjunker.de>
2014-12-19 14:52:22 +0100

    Read CHOST from portage configuration for crossdev target

--- roles/crossdev_getargs/files/getargs.sh
+++ roles/crossdev_getargs/files/getargs.sh
@@ -3,4 +3,5 @@ A="sys-devel/binutils" ; B=`equery l $A` ; BINUTILS_VER=`echo $B | cut -d- -f3-`
 A=`/usr/bin/gcc-config -c` ; B=`echo $A | cut -d- -f5` ; GCC_VER=`equery l sys-devel/gcc | grep $B | cut -d- -f3-`
 KERNEL_VER=`uname -r | sed s/-gentoo//`
 A="sys-libs/glibc" ; B=`equery l $A` ; LIBC_VER=`echo $B | cut -d- -f3-`
-echo "--b =$BINUTILS_VER --g =$GCC_VER --k =$KERNEL_VER --l =$LIBC_VER -t `uname -m`"
+source /etc/portage/make.conf
+echo "--b =$BINUTILS_VER --g =$GCC_VER --k =$KERNEL_VER --l =$LIBC_VER -t `uname -m` -t ${CHOST}"
