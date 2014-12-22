27f2f9bacd3139f971b42db2124dcdcbae50eba7
Stefan Junker <mail@stefanjunker.de>
2014-12-22 12:28:34 +0100

    handle amd64 qemu runs

--- roles/qemu_run/files/qemu_run.sh
+++ roles/qemu_run/files/qemu_run.sh
@@ -15,7 +15,7 @@
 #  -fsdev local,id=root,path=/var/tmp/embedux/rootfs/,security_model=mapped-file \
 #  -device virtio-9p-device,fsdev=root,mount_tag=/dev/root
 
-QEMU_AUDIO_DRV=none qemu-system-${TARGET_ARCH_SHORT} \
+QEMU_AUDIO_DRV=none qemu-system-${QEMU_ARCH} \
   -M ${MACHINE_ARG} \
   -m 3072 \
   -daemonize \
