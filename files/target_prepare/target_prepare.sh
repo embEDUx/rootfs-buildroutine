99fe67bd85b646061f4a8ee962b6ce008cca262f
Stefan Junker <mail@stefanjunker.de>
2014-12-23 15:53:17 +0100

    complete refactoring for better multi-arch handling

---
+++ files/target_prepare/target_prepare.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+## This code runs inside the rootfs
+DIR=$(dirname ${0})
+pushd ${DIR}
+
+# make.conf defaults
+cat make.conf.appendix >> /etc/portage/make.conf
+
+# Localization
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+echo de_DE.UTF-8 UTF-8 >> /etc/locale.gen
+/usr/sbin/locale-gen
+
+
+# SSH
+mkdir /root/.ssh/ -p
+chmod 700 /root/.ssh
+cp qemu.id.pub /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+/sbin/rc-update add sshd default
+cp send_signal_from_vm.py /etc/local.d/send_signal_from_vm.py.start
+
+
+# Portage
+emerge --sync
+emerge gentoolkit flaggie
+
+# Prepare binary packages
+mkdir /mnt/pkgdir
+chown portage:portage /mnt/pkgdir
+echo PKGDIR=\"\/mnt/pkgdir\" >> /etc/portage/make.conf
+
+popd
