7a2a89ab1ec02d51d12a0c9ecf36eded0bd56d0f
Stefan Junker <mail@stefanjunker.de>
2014-12-23 16:00:30 +0100

    fixing errors

---
+++ files/qemu_prepare/qemu_prepare.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+## This code runs inside the rootfs
+DIR=$(dirname ${0})
+pushd ${DIR}
+
+# fstab
+cp fstab /etc/fstab
+
+# Inittab
+echo f0:12345:respawn:/sbin/agetty -a root 115200 ttyAMA0 vt100 >> /etc/inittab
+
+# Networking
+ln -sf /etc/init.d/net.lo /etc/init.d/net.eth0
+/sbin/rc-update add net.eth0 default
+
+# Kernel sources (needed by some packages to build properly)
+tar -xa -C / -f linux-rootfs-addon.tar
+ln -sf /usr/src/linux-* /usr/src/linux
+cp linux.config /usr/src/linux/.config
+
+# Dist-CC
+emerge sys-devel/distcc
+echo FEATURES=\"\${FEATURES} distcc distcc-pump\" >> /etc/portage/make.conf
+echo "10.0.2.2,lzo,cpp" > /etc/distcc/hosts
+
+WRAPPER_BASE=$(ls -1 /usr/lib/distcc/bin/ | grep -o ".*-" | uniq)
+cat << EOF > /usr/lib/distcc/bin/${WRAPPER_BASE}wrapper
+#!/bin/bash
+exec /usr/lib/distcc/bin/${WRAPPER_BASE}g\${0:\$[-2]} "\$@"
+EOF
+chmod +x /usr/lib/distcc/bin/${WRAPPER_BASE}wrapper
+rm /usr/lib/distcc/bin/{c++,g++,gcc,cc}
+for cmd in c++ g++ gcc cc; do
+    ln -s ${WRAPPER_BASE}wrapper /usr/lib/distcc/bin/${cmd}
+done
+
+popd
